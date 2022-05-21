559f04d662e717e93faf4f7265b7fb9fd5d5d3e4
Chris Leensvaart <cleensvaart@gmail.com>
2022-05-20 23:34:52 -0500

    CSS Tweak

--- pages/library.js
+++ pages/library.js
@@ -3,7 +3,7 @@ import styles from '../styles/MyLibrary.module.css';
 export default function Library() {
   return (
     <div>
-      <h1>My Library</h1>
+      <h3>My Library</h3>
     </div>
   );
 }
